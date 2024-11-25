b8c33334aa209f57712bda3d68f0e5452bd34d1c
Julio Cesar de Matos <github@jcdematos.com>
2024-11-25 18:43:32 -0300

    Add lua language server

--- init.lua
+++ init.lua
@@ -20,11 +20,38 @@ vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
 -- LSP 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
 local lspconfig = require('lspconfig')
 
 -- LSP - Bash Language Server
-require'lspconfig'.bashls.setup{}
+lspconfig.bashls.setup{}
+
+-- LSP - Lua Language Server
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          'vim',
+          'require'
+        },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 require'cmp'.setup {
   sources = {
