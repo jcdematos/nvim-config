1aab80876961411278ab7a747474b3fa587db5ac
jcdematos <github@jcdematos.com>
2025-11-21 17:08:39 -0300

    Migrate to Lazy.vim

--- ftplugin/sh.vim
+++
@@ -1,7 +0,0 @@
-setlocal shiftwidth=4
-
-nnoremap ex :!bash % #
-
-" Comment and uncomment
-nnoremap <leader>c ^i# <ESC>
-nnoremap <leader>uc ^xx
--- lua/config/init.lua
+++
@@ -1,7 +0,0 @@
-local opt = vim.opt
-
--- vim.o.timeoutlen = 300 -- time between keys of commands
-vim.g.mapleader = " " 
-
-opt.number = true
-opt.relativenumber = true
